e8db0277910d21b2da8747e622a320ed213bfc3e
prayansh910 <prayanshgupta910@gmail.com>
2026-09-17 00:42:03 +0530

    Time: 5 ms (67.91%), Space: 13.8 MB (20.3%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        k=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[k]=nums[i]
+                k+=1
+        while k<len(nums):
+            nums[k]=0
+            k+=1
+
+        return nums
